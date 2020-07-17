d8995f0626e6eb8c16aead82fa2cd8197255f9f1
Ghadage <rahul.ghadage@atos.net>
2020-07-17 15:05:36 +0530

    Added WebClient call for country web api

--- country-details-client/src/main/java/com/arya/microservices/countryclient/controller/CountryClientController.java
+++ country-details-client/src/main/java/com/arya/microservices/countryclient/controller/CountryClientController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class CountryClientController {
 
     @Autowired
     private RestTemplate template;
+    
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+    
 
+    
     @Value("${server.port}")
     private String port;
 
@@ -22,6 +28,7 @@ public class CountryClientController {
     @GetMapping("/{country}")
     public Object getCountryData(@PathVariable String country) {
 
+    	Object response = null;
         System.out.println("Fetching data from country : " + country);
 
         System.out.println("\n\n*********************************************");
@@ -30,8 +37,18 @@ public class CountryClientController {
 
         if (country.endsWith("ico"))
             throw new RuntimeException("Don't know how in second call getting country name as : " + country);
+        
+        
+		response = webClientBuilder.build()
+			.get().uri(COUNTRY_API + country)
+			.retrieve()
+			.bodyToMono(Object.class)
+			.block();
+		
+		//response  = template.getForEntity(COUNTRY_API + country, Object.class);
+        
 
-        return template.getForEntity(COUNTRY_API + country, Object.class);
+        return response;
 
     }
 }
